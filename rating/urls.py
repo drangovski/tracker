98abc03d7d7c0d9954b8ba8608918b561c033ea5
Bojan Drangovski <bojandrango@gmail.com>
2022-02-21 15:56:44 +0100

    Update urls.py

--- rating/urls.py
+++ rating/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('dashboard/', views.dashboard, name="dashboard"),
+	path('', views.dashboard, name="dashboard"),
 	path('games/', views.games, name="games"),
 	path('games/<slug:slug>', views.ratings, name="ratings"),
 	path('renew/', views.renew, name="renew"),
